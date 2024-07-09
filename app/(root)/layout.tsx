51b6f2da415cc1b60b477920c78f894259e827ac
biooids <biooidsbiooid@gmail.com>
2024-07-09 04:20:52 +0200

    "Updated dependencies and added new ones, including @radix-ui/react-toast and @stream-io/node-sdk, and modified layout.tsx, MeetingTypeList.tsx, and package-lock.json files."

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
-import React, { ReactNode } from "react";
+import StreamVideoProvider from "@/providers/StreamClientProviders";
+import { ReactNode } from "react";
 
-function RootLayout({ children }: { children: ReactNode }) {
-  return <main>{children}</main>;
-}
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
 
 export default RootLayout;
